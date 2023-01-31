bf98e41d3f39740a26efe2cf6cee93b6992ebd8d
Thxndered <LilthunderYT@gmail.com>
2023-01-30 19:53:06 -0500

    I worked on the ControlMode's for the turret and made a new and very epic DriveTurret command The DriveTurret command uses PercentOutput and it only exists to set the motor to zero probably.

--- src/main/java/org/team2168/subsystems/Turret.java
+++ src/main/java/org/team2168/subsystems/Turret.java
@@ -4,6 +4,7 @@
 
 package org.team2168.subsystems;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -54,6 +55,32 @@ public class Turret extends SubsystemBase {
 
   private static double ticksToDegrees(double ticks) {
     return (ticks / TICKS_PER_TURRET_ROTATION) * 360.0;
+  }
+
+  private static double degreesToEncoderTicks(double degrees) {
+    return (degrees/360.0) * TICKS_PER_TURRET_ROTATION;
+  }
+
+  private static double ticksPer100msToDegreesPerSecond(double ticks) {
+    return ticksToDegrees(ticks) * 10.0;
+  }
+
+  public static double degreesPerSecondToTicksPer100ms(double degrees) {
+    return degreesToEncoderTicks(degrees) / 10.0;
+  }
+
+  public void setRotationDegrees(double degrees) {
+    var demand = MathUtil.clamp(degrees, ticksToDegrees(MIN_ROTATION_TICKS), ticksToDegrees(MAX_ROTATION_TICKS));
+    setpoint = degrees;
+    turretMotor.set(ControlMode.MotionMagic, degreesToEncoderTicks(demand));
+  }
+
+  public void setVelocity(double degrees) {
+    turretMotor.set(ControlMode.Velocity, degreesPerSecondToTicksPer100ms(degrees));
+  }
+
+  public void setSpeed(double speed) {
+    turretMotor.set(ControlMode.PercentOutput, speed);
   }
 
   @Override
