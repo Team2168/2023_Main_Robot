2e415e28c6bb33223c7953ef2d872f2db869477d
Katie Lam <katielam@team2168.org>
2023-01-31 19:53:17 -0500

    Finished adding motor config stuff

--- src/main/java/org/team2168/subsystems/Arm.java
+++ src/main/java/org/team2168/subsystems/Arm.java
@@ -7,6 +7,9 @@ package org.team2168.subsystems;
 import org.team2168.Constants;
 import org.team2168.utils.TalonFXHelper;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,6 +20,7 @@ public class Arm extends SubsystemBase {
   private TalonFXHelper back_motor;
 
   // Current limit configuration
+  private SupplyCurrentLimitConfiguration talonCurrentLimit;
   private static final boolean ENABLE_CURRENT_LIMIT = true;
   private static final double CONTINUOUS_CURRENT_LIMIT = 20; // amps TODO: update limit
   private static final double TRIGGER_THRESHOLD_LIMIT = 30; // amp TODO: update limit
@@ -26,10 +30,14 @@ public class Arm extends SubsystemBase {
   private static final double TICKS_PER_REV = 2048;
   private static final double GEAR_RATIO = 0.0; //TODO: update value, teeth/diameter
 
+  private static final double kPeakOutput = 1.0;
   private static final int kPIDLoopIdx = 0;
   private static final int kTimeoutMs = 30;
   private static boolean kSensorPhase = false;
   private static TalonFXInvertType kMotorInvert = TalonFXInvertType.Clockwise;
+
+  private static final double ACCELERATION_LIMIT = 0.0; //TODO: update value after testing
+  private static final double CRUISE_VELOCITY_LIMIT = 0.0; //TODO: update value after testing
  
   private static Arm instance = null;
 
@@ -59,6 +67,31 @@ public class Arm extends SubsystemBase {
 
     front_motor.configFactoryDefault();
     front_motor.configNeutralDeadband(NEUTRAL_DEADBAND);
+    front_motor.setNeutralMode(NeutralMode.Brake);
+
+    front_motor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, kPIDLoopIdx, kTimeoutMs);
+    front_motor.setSensorPhase(kSensorPhase);
+    front_motor.setInverted(kMotorInvert);
+
+    front_motor.configNominalOutputForward(0, kTimeoutMs);
+    front_motor.configNominalOutputReverse(0, kTimeoutMs);
+    front_motor.configPeakOutputForward(kPeakOutput, kTimeoutMs);
+    front_motor.configPeakOutputReverse(-kPeakOutput, kTimeoutMs);
+
+    front_motor.config_kP(kPIDLoopIdx, kP, kTimeoutMs);
+    front_motor.config_kI(kPIDLoopIdx, kI, kTimeoutMs);
+    front_motor.config_kD(kPIDLoopIdx, kD, kTimeoutMs);
+    front_motor.config_kF(kPIDLoopIdx, kF, kTimeoutMs);
+    front_motor.configMotionAcceleration(ACCELERATION_LIMIT);
+    front_motor.configMotionCruiseVelocity(CRUISE_VELOCITY_LIMIT);
+    front_motor.configAllowableClosedloopError(0, ACCELERATION_LIMIT, kTimeoutMs);
+    
+    talonCurrentLimit = new SupplyCurrentLimitConfiguration(ENABLE_CURRENT_LIMIT, CONTINUOUS_CURRENT_LIMIT, 
+      TRIGGER_THRESHOLD_LIMIT, TRIGGER_THRESHOLD_TIME);
+    
+    front_motor.configSupplyCurrentLimit(talonCurrentLimit);
+
+    front_motor.configClosedLoopStatusFrameRates();
   }
 
   @Override
