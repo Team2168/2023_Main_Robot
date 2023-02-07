1d32422c4e6521955fe3725b85951b1e50d819e3
Shubbii <shubbiievii@gmail.com>
2023-02-06 20:33:37 -0500

    Created new commands Finished ArcadeDrive() started working on PathPlanner integration

---
+++ src/main/java/org/team2168/commands/auto/DebugPathPlanner.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team2168.commands.auto;
+
+import org.team2168.subsystems.Drivetrain;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+
+public class DebugPathPlanner extends CommandBase {
+  /** Creates a new DebugPathPlanner. */
+  Drivetrain drivetrain;
+  RamseteCommand rCommand;
+  String pathname;
+
+  public DebugPathPlanner(Drivetrain drivetrain, String pathname) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.drivetrain = drivetrain;
+    this.pathname = pathname;
+  }
+
+  rCommand = new RamseteCommand(
+    
+  )
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
