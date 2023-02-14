7e656efc0b3451754258a2525e52f280355aa14e
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-02-13 16:04:55 -0800

    2023 import of oblarg

--- src/main/java/org/team2168/RobotContainer.java
+++ src/main/java/org/team2168/RobotContainer.java
@@ -20,6 +20,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import io.github.oblarg.oblog.Logger;
 import io.github.oblarg.oblog.annotations.Config;
 import io.github.oblarg.oblog.annotations.Log;
+import io.github.oblarg.oblog.Logger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
