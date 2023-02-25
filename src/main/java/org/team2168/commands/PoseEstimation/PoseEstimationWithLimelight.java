013dff586e18a03d4f8048b0cc590480042cf42d
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-02-25 10:19:17 -0800

    added Vector Matrix to Differential PoseEstimator

--- src/main/java/org/team2168/commands/PoseEstimation/PoseEstimationWithLimelight.java
+++ src/main/java/org/team2168/commands/PoseEstimation/PoseEstimationWithLimelight.java
@@ -6,11 +6,17 @@ package org.team2168.commands.PoseEstimation;
 
 import org.team2168.subsystems.Limelight;
 
+import edu.wpi.first.math.MatBuilder;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.estimator.UnscentedKalmanFilter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -18,12 +24,14 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class PoseEstimationWithLimelight extends CommandBase {
   private Limelight lime;
   private DifferentialDrivePoseEstimator poseEstimator  = new DifferentialDrivePoseEstimator(null, null, 0, 0, null);
-  private DifferentialDrivePoseEstimator estimator = new DifferentialDrivePoseEstimator(null, null, 0, 0, null, null, null)
+  private Matrix<N3,N1> matrix = VecBuilder.fill(0.01, 0.02, 0.03);
+
   public PoseEstimationWithLimelight(Limelight lime) {
     this.lime = lime;
     // Use addRequirements() here to declare subsystem dependencies.
     poseEstimator.addVisionMeasurement(new Pose2d(new Translation2d(lime.botPoseArrayTwo[1], lime.botPoseArrayTwo[2]), new Rotation2d(Units.radiansToDegrees(lime.getOffsetX()))), Timer.getFPGATimestamp());
     poseEstimator.setVisionMeasurementStdDevs(null);
+
     
   }
 
