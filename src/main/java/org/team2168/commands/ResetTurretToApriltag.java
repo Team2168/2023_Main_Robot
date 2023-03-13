228c997de07fe5fc88d93f94d703d00d7e7399b2
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-03-13 16:41:26 -0700

    added command that resets turret to apriltag from button press

--- src/main/java/org/team2168/commands/ResetTurretToApriltag.java
+++ src/main/java/org/team2168/commands/ResetTurretToApriltag.java
@@ -4,12 +4,26 @@
 
 package org.team2168.commands;
 
+import org.team2168.commands.PoseEstimation.PoseEstimationWithLimelight;
+import org.team2168.subsystems.Limelight;
+import org.team2168.subsystems.Turret;
+
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ResetTurretToApriltag extends CommandBase {
-  /** Creates a new ResetTurretToApriltag. */
-  public ResetTurretToApriltag() {
+  
+  public Limelight lime;
+  public Turret turret;
+  public PoseEstimationWithLimelight poseEstimator;
+
+
+  
+  public ResetTurretToApriltag(Limelight lime, Turret turret, PoseEstimationWithLimelight poseEstimator) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.lime = lime;
+    this.turret = turret;
+    this.poseEstimator = poseEstimator;
   }
 
   // Called when the command is initially scheduled.
@@ -18,7 +32,15 @@ public class ResetTurretToApriltag extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    //make code that checks what apriltag I.D we are at based on pose with switch case, or if else.
+    double diffX = poseEstimator.getPose().getX() - lime.getApriltagDimensionsFromFidicualId().getX();
+    double diffY = poseEstimator.getPose().getY() - lime.getApriltagDimensionsFromFidicualId().getY();
+
+    double finalAngle = turret.getTurretAngle() + Units.radiansToDegrees(Math.atan(diffY/diffX));
+
+    turret.setRotationDegrees(finalAngle);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
