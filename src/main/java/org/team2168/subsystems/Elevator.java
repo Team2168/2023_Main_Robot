ef92e32333d99a07a67006c755f2a395b3c6052b
Naman <patel.naman3@gmail.com>
2023-03-13 20:07:42 -0400

    Tried to fix the broken simulation (didn't work)

--- src/main/java/org/team2168/subsystems/Elevator.java
+++ src/main/java/org/team2168/subsystems/Elevator.java
@@ -21,6 +21,7 @@ import org.team2168.Constants;
 import org.team2168.Constants.ElevatorMotors;
 import org.team2168.utils.TalonFXHelper;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotController;
@@ -110,7 +111,7 @@ public class Elevator extends SubsystemBase {
 
     elevatorMotor.configFactoryDefault();
    
-    elevatorSim = new ElevatorSim(DCMotor.getFalcon500(1), GEAR_RATIO, CARRIAGE_MASS_KG, Units.inchesToMeters(SPROCKET_RADIUS), Units.inchesToMeters(MIN_HEIGHT_INCHES), Units.inchesToMeters(MAX_HEIGHT_INCHES), kSensorPhase, null);
+    elevatorSim = new ElevatorSim(DCMotor.getFalcon500(1), GEAR_RATIO, CARRIAGE_MASS_KG, Units.inchesToMeters(SPROCKET_RADIUS), Units.inchesToMeters(MIN_HEIGHT_INCHES), Units.inchesToMeters(MAX_HEIGHT_INCHES), kSensorPhase, VecBuilder.fill(0.1));
     elevatorMotorSim = elevatorMotor.getSimCollection();
 
     //public EncoderSim encoderSim = new EncoderSim(encoder);
