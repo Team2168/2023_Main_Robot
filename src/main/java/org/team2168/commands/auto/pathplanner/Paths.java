9f565d56cc6ab3266e5e0489730ff25ab366a7d2
Naman <patel.naman3@gmail.com>
2023-05-30 19:21:02 -0400

    Made the Score_Two auto better and I segemented it better and it should work better now I think so it's more in line with the other autos.

--- src/main/java/org/team2168/commands/auto/pathplanner/Paths.java
+++ src/main/java/org/team2168/commands/auto/pathplanner/Paths.java
@@ -21,7 +21,8 @@ public class Paths {
     public final Trajectory path_score_low_1;
     public final Trajectory path_score_low_2;
     public final Trajectory engage_station;
-    public final Trajectory path_score_two_nodes;
+    public final Trajectory path_score_two_nodes_part1;
+    public final Trajectory path_score_two_nodes_part2;
 
     private static Paths instance = null;
 
@@ -35,7 +36,8 @@ public class Paths {
         path_score_low_1 = getTrajectory("score_low_wne_1", false);
         path_score_low_2 = getTrajectory("score_low_wne_2", false);
         engage_station = getTrajectory("engage_station", false);
-        path_score_two_nodes = getTrajectory("score_two", false);
+        path_score_two_nodes_part1 = getTrajectory("score_two_part1", false);
+        path_score_two_nodes_part2 = getTrajectory("score_two_part2", false);
 
         System.out.println("******* Finish generating autos *******");
     }
--- src/main/java/org/team2168/commands/auto/pathplanner/Score_Two.java
+++ src/main/java/org/team2168/commands/auto/pathplanner/Score_Two.java
@@ -4,17 +4,18 @@
 
 package org.team2168.commands.auto.pathplanner;
 
+import org.team2168.commands.drivetrain.ArcadeDrive;
 import org.team2168.subsystems.Drivetrain;
 import org.team2168.utils.PathUtil;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-public class Score_Two extends CommandBase {
+public class Score_Two extends SequentialCommandGroup {
   /** Creates a new Score_Two. */
-  public class EngageChargingStation extends SequentialCommandGroup {
     /** Creates a new EngageChargingStation. */
-    public EngageChargingStation(Drivetrain d) {
+    public Score_Two(Drivetrain d) {
       // Add your commands in the addCommands() call, e.g.
       // addCommands(new FooCommand(), new BarCommand());
       Drivetrain drivetrain = d;
@@ -22,7 +23,10 @@ public class Score_Two extends CommandBase {
       Paths paths = Paths.getInstance();
       //DriverStation.Alliance driverstationColor = DriverStation.getAlliance(); //meant for blue alliance not red alliance
   
-      addCommands(PathUtil.getPathCommand(paths.path_score_two_nodes, drivetrain, PathUtil.InitialPathState.DISCARDHEADING));
-    }
+      addCommands(PathUtil.getPathCommand(paths.path_score_two_nodes_part1, drivetrain, PathUtil.InitialPathState.DISCARDHEADING));
+      addCommands(new ArcadeDrive(drivetrain, () -> 0.3, () -> 0.0).withTimeout(0.2),
+      new WaitCommand(0.5),
+      PathUtil.getPathCommand(paths.path_score_two_nodes_part2, drivetrain, PathUtil.InitialPathState.DISCARDHEADING),
+      new ArcadeDrive(drivetrain, () -> -0.3, () -> 0.0).withTimeout(1.4));
   }
 }
